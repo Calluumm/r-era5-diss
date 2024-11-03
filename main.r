76861cb8c5b10439adbd8d0804fa87ef8ab5c7a5
Callum Winder <69708064+Calluumm@users.noreply.github.com>
2024-11-03 18:59:59 +0000

    Update main.r
    Added the 2 new plotters and support for timestamping full ncdfs

--- main.r
+++ main.r
@@ -8,20 +8,20 @@ source("variables.r")
 source("plotter.r")
 #employ parallel processing this is so slow i might cry
 
-ph <- "FILE PATH FOR DATA TO GO"
+ph <- "FILE_PATH"
 vr <- c("temperature")
-yrs <- 1940:2024
+yrs <- 2024
 mn <- sprintf("0%d",5:5)
 dy <- c(sprintf("0%d",1:9),10:31)
 tm <- "00:00"
 are <- "77.22/15.63/77.23/15.64"
 cds.key <- "API KEY"
 wf_set_key(user ="USERNAME", key = cds.key)
-nc_file_path2 <- "direct path to a second nc_file for comparative plots"
+nc_file_path2 <- "SECONDARY NCDF FILE PATH"
 # ALL THESE VARIABLES ARE EXAMPLES CHANGE BASED ON WHAT YOU WANT#
 # land data only takes 1 year/1 month but variable days
 
-choice <- readline(prompt = "Choose an option \n1: era5_data \n2: era5_land_data \n3: continue without downloading ")
+choice <- readline(prompt = "Choose an option \n1: era5_data \n2: era5_land_data \n3: continue without downloading \n")
 if (choice == "1") {
   download_era5_data(yrs, mn, tm, vr, ph, are)
 } else if (choice == "2") {
@@ -36,7 +36,7 @@ if (choice == "1") {
 #Automatic entry of file path
 #file_path <- get_most_recent_nc_file(ph)
 # Manual entry of file path
-file_path <- paste0(ph, "MANUAL FILE NAME.nc")
+file_path <- paste0(ph, "\\MANUAL NCDF.nc")
 
 print("Starting to process the NetCDF file...")
 nc_data <- nc_open(file_path)
@@ -59,10 +59,23 @@ if (!vr %in% names(variable_mappings)) {
   stop("Invalid variable name. Please check the variable_mappings.")
 }
 retrieved_data <- ncvar_get(nc_data, variable_mappings[[vr]])
+
+# Check if the retrieved data has a time dimension
+if (length(dim(retrieved_data)) == 3) {
+  # If it has a time dimension, reshape the data to associate each data point with the correct timestamp
+  retrieved_data <- array(retrieved_data, dim = c(dim(retrieved_data)[1] * dim(retrieved_data)[2], dim(retrieved_data)[3]))
+  valid_time_subset <- rep(valid_time, each = dim(retrieved_data)[1])
+} else if (length(dim(retrieved_data)) == 2) {
+  # If it has no time dimension, use the data as is
+  retrieved_data <- retrieved_data
+} else {
+  stop("Error: retrieved_data does not have the expected number of dimensions")
+}
+
 nc_close(nc_data)
 df <- data.frame(time = valid_time_subset, value = retrieved_data)
 
-cat("Choose plot type:\n1. Line Plot\n2. Smooth Plot\n3. SD Yearly Plot \n4. Shapefile Plot (only for temperature/single timeframes right now)\n5. Plot shapefile difference between 2 timeframes\n")
+cat("Choose plot type:\n1. Line Plot\n2. Smooth Plot\n3. SD Yearly Plot \n4. Shapefile Plot (only for temperature/single timeframes right now)\n5. Plot shapefile difference between 2 individual timeframes\n6. Plot a shapefile comparison between decades\n7. Plot a shapefile average of an entire ncdf\n")
 plot_choice <- as.integer(readLines(con = stdin(), n = 1))
 
 
@@ -91,10 +104,18 @@ if (plot_choice == 1) {
   plot <- plot_shapefile(file_path, vr)
 }else if (plot_choice == 5) {
   plot <- plot_shapefile_difference(file_path, nc_file_path2, vr)
+} else if (plot_choice ==6) {
+  cat("Enter the start of decade 1 (e.g. 1940): \n")
+  start_decade1 <- as.integer(readLines(con = stdin(), n = 1))
+  cat("Enter the start of decade 2 (e.g. 1950): \n")
+  start_decade2 <- as.integer(readLines(con = stdin(), n = 1))
+  plot <- plot_shapefile_ranged(file_path, vr, start_decade1, start_decade2)
+} else if (plot_choice == 7) {
+  plot <- plot_entire_average(file_path, vr)
+
 } else {
   stop("Invalid plot choice.")
 }
 
 print(plot)
 stop("stopped")
-
