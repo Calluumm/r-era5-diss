95cf50eda375a153c5fae91d96a1ac4bdfd74ec3
Callum Winder <69708064+Calluumm@users.noreply.github.com>
2025-02-10 21:32:21 +0000

    Update image combiner.py
    changed text additions to PIL

--- image combiner.py
+++ image combiner.py
@@ -1,3 +1,4 @@
+from PIL import Image, ImageDraw, ImageFont
 import cv2
 import numpy as np
 import os
@@ -37,27 +38,49 @@ line_thickness = 15
 #cv2 line is essentially just (file, start, end, colour, thickness)
 cv2.line(combined_image_ba, (2*width, height//2), (2*width + annotation_width//4, height//4), line_color, line_thickness)
 cv2.line(combined_image_ba, (2*width, 3*height//2), (2*width + annotation_width//4, 1*height//4), line_color, line_thickness)
-font = cv2.FONT_HERSHEY_SIMPLEX
-font_scale = 2.0
-font_color = (0, 0, 0)
-font_thickness = 10
-font_scale2 = 1.5
-font_thickness2 = 5
-# the setup is (file, text, position on image, font, font size, colour, thickness)
-cv2.putText(combined_image_ba, 'U + V components', (2*width + annotation_width//4 + 10, height//2 - 500), font, font_scale, font_color, font_thickness)
+#make it PIL so not constrained to cv2 fonts
+combined_image_ba_pil = Image.fromarray(cv2.cvtColor(combined_image_ba, cv2.COLOR_BGR2RGB))
+draw = ImageDraw.Draw(combined_image_ba_pil)
+
+font_path = "arial.ttf" #or any old font
+font = ImageFont.truetype(font_path, 80)
+font2 = ImageFont.truetype(font_path, 60)
+
+draw.text((2*width + annotation_width//4 - 50, height//2 - 700), 'U + V trends', font=font, fill=(0, 0, 0))
 multi_line_text = [
-"lines look like this", 
-"there is no support from cv2", 
-"for line splits with \n"
+    'Increased windspeeds from',
+    'the X and X.',
+    'It\'s not clear if these',
+    'directional increases are',
+    'independant of one another.'
 ]
-y0 = height//4 + 50
-dy = 40 
+y0 = height//2 - 625
+dy = 60 
 for i, line in enumerate(multi_line_text):
     y = y0 + i * dy
-    cv2.putText(combined_image_ba, line, (2*width + annotation_width//4 + 10, y), font, font_scale2, font_color, font_thickness2)
+    draw.text((2*width + annotation_width//4 - 50, y), line, font=font2, fill=(0, 0, 0))
 
+draw.text((2*width + annotation_width//4 - 50, height//2 + 600), 'Correlation Coefficients', font=font, fill=(0, 0, 0))
+multi_line_text2 = [
+    'Temperature: coeff',
+    'U Component: coeff',
+    'V Component: coeff',
+    'Precip. Spearmans: coeff'
+]
+y01 = height//2 + 675
+dy = 60 
+for i, line in enumerate(multi_line_text2):
+    y1 = y01 + i * dy
+    draw.text((2*width + annotation_width//4 - 50, y1), line, font=font2, fill=(0, 0, 0))
 
-filename = 'summer_combined.jpg'
+# back to cv2
+combined_image_ba = cv2.cvtColor(np.array(combined_image_ba_pil), cv2.COLOR_RGB2BGR)
+
+filename = 'summer_combined_f.jpg'
+cv2.imwrite(os.path.join(ph, filename), combined_image_ba)
+
+print("Combined image saved as", filename)
+filename = 'NAME.jpg or png'
 cv2.imwrite(os.path.join(ph, filename), combined_image_ba)
 
 print("Combined image saved as", filename)
