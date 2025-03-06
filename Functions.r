4c4e563a6fa27b42ff0ceae9f6e4055fb736821b
Callum Winder <69708064+Calluumm@users.noreply.github.com>
2025-03-06 19:49:15 +0000

    Update Functions.r
    removed unncessary functions

--- Functions.r
+++ Functions.r
@@ -1,15 +1,3 @@
-#  EOF Function
-perform_eof <- function(data_matrix) {
-  eof_result <- svd(data_matrix)
-  return(eof_result)
-}
-
-# PCA Function
-perform_pca <- function(data_matrix) {
-  pca_result <- prcomp(data_matrix, scale. = TRUE)
-  return(pca_result)
-}
-
 library(foreach)
 library(ecmwfr)
 
